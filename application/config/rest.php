2b7f7215837575782d78489599e943e32e469b96
Krishnendu <krishnendudalui@gmail.com>
2024-09-01 11:54:12 +0530

    controller,route,postman

--- application/config/rest.php
+++ application/config/rest.php
@@ -298,7 +298,7 @@ $config['rest_database_group'] = 'default';
 | The table name in your database that stores API keys
 |
 */
-$config['rest_keys_table'] = 'keys';
+$config['rest_keys_table'] = 'api_keys';
 
 /*
 |--------------------------------------------------------------------------
@@ -323,7 +323,7 @@ $config['rest_keys_table'] = 'keys';
 |   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 |
 */
-$config['rest_enable_keys'] = false;
+$config['rest_enable_keys'] = true;
 
 /*
 |--------------------------------------------------------------------------
@@ -334,7 +334,7 @@ $config['rest_enable_keys'] = false;
 | column name to match e.g. my_key
 |
 */
-$config['rest_key_column'] = 'key';
+$config['rest_key_column'] = 'my_key';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/controllers/api/ApiDemoController.php
@@ -0,0 +1,27 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed'); 
+ 
+require APPPATH . 'libraries/RestController.php'; 
+require APPPATH . 'libraries/Format.php'; 
+use chriskacerguis\RestServer\RestController; 
+use chriskacerguis\RestServer\Format; 
+class ApiDemoController extends RestController 
+{ 
+    public function __construct() 
+    { 
+        parent::__construct(); 
+        // Load any models, libraries, etc. that you'll need here. 
+    } 
+ 
+    public function index_get() 
+    { 
+        echo "I am restful API"; 
+      
+    } 
+ 
+  
+} 
+?> 
+ 
+
+
